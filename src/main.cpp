89e72151827124ac37b56bb5c8d948155ed0871e
MattYoung-UoY <mjy503@york.ac.uk>
2022-10-08 00:54:23 +0100

    Added Colour and Ray Headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "colour.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -14,11 +17,8 @@ int main() {
             auto g = double(j) / (image_height-1);
             auto b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            colour pixel_colour(r, g, b);
+            write_colour(std::cout, pixel_colour);
         }
     }
 
